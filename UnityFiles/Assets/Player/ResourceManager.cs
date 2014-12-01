a16c6617be3da07799128bb91d9649ffa687b10a
Matti Ruha <mttrha@gmail.com>
2014-12-01 15:50:00 +0200

    Real zoom and rotation scaling
    Change y axel zoom to zoom in on target. Scale rotation so that faster
    mouse movements get faster response from camera movement.

--- UnityFiles/Assets/Player/ResourceManager.cs
+++ UnityFiles/Assets/Player/ResourceManager.cs
@@ -7,8 +7,11 @@ namespace RTS{
 	 	public static float ScrollSpeed{ get{ return 25;}}
 	 	public static float RotateSpeed{ get{ return 40;}}
 	 	public static float ScrollWidth{ get{ return 15;}}
+	 	
 	 	public static float MinCameraHeight{ get{ return 10;}}
 		public static float MaxCameraHeight{ get{ return 40;}}
+		public static float CamMinDistance{ get{ return 10;}}
+		
 		public static float RotateAmount{ get{ return 10;}}
 		public static float RotateScale{ get{ return 5;}}
 		
--- UnityFiles/Assets/Player/UserInput.cs
+++ UnityFiles/Assets/Player/UserInput.cs
@@ -10,7 +10,34 @@ public class UserInput : MonoBehaviour
 	private static float hypot;
 	private static float initAngle;
 
-	// private methods
+	// zoom Camera
+	private void ZoomCamera ()
+	{
+		Vector3 camOrigin = Camera.main.transform.position;
+		
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2,0));
+		RaycastHit rayHit;
+		
+		if(Physics.Raycast(ray,out rayHit,Mathf.Infinity))
+		{
+			// vertical movement
+			float mouseWheel = ResourceManager.ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
+			
+			if(mouseWheel != 0)
+			{
+				Debug.Log ("mouseWheel: " + mouseWheel);
+			
+				// restrict y with min and max
+				if(Vector3.Distance(camOrigin,rayHit.point) > ResourceManager.CamMinDistance || mouseWheel < 0)
+				{
+					// move Camera
+					Camera.main.transform.position = Vector3.MoveTowards(camOrigin,rayHit.point, mouseWheel * Time.deltaTime * ResourceManager.ScrollSpeed);			
+				}
+			}
+		}		
+	}
+
+	// move Camera scrolling
 	private void MoveCamera()
 	{
 		if(!Input.GetMouseButton(1))
@@ -47,7 +74,7 @@ public class UserInput : MonoBehaviour
 			movement.y = 0;
 			
 			// vertical movement
-			movement.y -= ResourceManager.ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
+			//movement.y -= ResourceManager.ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
 			
 			// Calculate the desired camera position
 			Vector3 origin = Camera.main.transform.position;
@@ -56,17 +83,7 @@ public class UserInput : MonoBehaviour
 			destination.x += movement.x;
 			destination.y += movement.y;
 			destination.z += movement.z;
-			
-			// restrict y with min and max
-			if(destination.y < ResourceManager.MinCameraHeight)
-			{
-				destination.y = ResourceManager.MinCameraHeight;
-			}
-			else if(destination.y > ResourceManager.MaxCameraHeight)
-			{
-				destination.y = ResourceManager.MaxCameraHeight;
-			}
-			
+					
 			// move the Camera
 			if(destination != origin)
 			{
@@ -82,9 +99,11 @@ public class UserInput : MonoBehaviour
 		RaycastHit rayHit;
 		Vector3 hitCoord;
 		
+#if DEBUG		
 		// debug ray
 		Debug.DrawRay(ray.origin, ray.direction * 400, Color.yellow);
-		
+#endif
+
 		// check if ray hits plane
 		if(Physics.Raycast(ray,out rayHit,Mathf.Infinity))
 		{
@@ -108,9 +127,10 @@ public class UserInput : MonoBehaviour
 				hypot = Vector3.Distance(temp,new Vector3(0,0,0)); // calculate hyptenuse, y in pboth are the same os diff is only in x,z
 
 				initAngle = Mathf.Atan2(temp.z,temp.x)*(180/Mathf.PI);
-
+#if DEBUG
 				Debug.Log("Set inits: " + initCoord + " , " + initMouseX + " , " + initAngle);
 				Debug.DrawLine(initCoord, camOrigin, Color.yellow,10);
+#endif
 			}
 			else // start rotate around y axel in init coord 
 			{
@@ -121,19 +141,21 @@ public class UserInput : MonoBehaviour
 				{
 					Vector3 camOrigin = Camera.main.transform.position;
 					Vector3 camDest;
-					
+#if DEBUG					
 					Debug.Log("xDiff: " + xDiff + " hypot: " + hypot + " initAngle: " + initAngle);
-					
+#endif					
 					camDest.x = hypot * Mathf.Cos ((xDiff + initAngle)*Mathf.PI/180) + initCoord.x;
 					camDest.z = hypot * Mathf.Sin ((xDiff + initAngle)*Mathf.PI/180) + initCoord.z;
 					camDest.y = camOrigin.y;
+#if DEBUG
 					//Debug.Log ("cos: " + Mathf.Cos ((xDiff + camAngOrig.y)*Mathf.PI/180) + " sin: " + Mathf.Sin ((xDiff + camAngOrig.y)*Mathf.PI/180));
 					Debug.Log("InitCoord: " + initCoord + " CamOrig: " + camOrigin + " CamDest: " + camDest);
-					
-					// move camera to x,z 
-					Camera.main.transform.position = Vector3.MoveTowards(camOrigin,camDest,Time.deltaTime * ResourceManager.RotateSpeed);
+#endif			
+					// move camera to x,z, scaled with mousePointer speed
+					Camera.main.transform.position = Vector3.MoveTowards(camOrigin,camDest,Time.deltaTime * ResourceManager.RotateSpeed * Mathf.Abs(xMouseScreen - initMouseX));
+#if DEBUG
 					Debug.DrawLine(initCoord, camDest, Color.red,10);
-					
+#endif			
 					// turn camera in new location x,z towards the initCoord/target
 					Camera.main.transform.LookAt(initCoord);
 				}				
@@ -152,6 +174,7 @@ public class UserInput : MonoBehaviour
 	void Update () 
 	{
 		MoveCamera ();
+		ZoomCamera ();
 		
 		// ALT + mouse to rotate the camera
 		if((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetMouseButton(1))
